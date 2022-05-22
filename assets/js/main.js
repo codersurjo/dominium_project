bc1740d88ecb829d84660ed3bbefeef0bebf95c8
Arun chandra roy <48654733+codersurjo@users.noreply.github.com>
2022-05-22 23:22:42 +0600

    22-5-22 Last comment

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    // Hero_car
+    $('.hero_car').owlCarousel({
+        loop: true,
+        nav: false,
+        autoplay: false,
+        autoplayHoverPause: true,
+        items: 1,
+    });
+})
