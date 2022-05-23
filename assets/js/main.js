4c8422b43b258e94c64ea526691e5c2067932ce6
Arun chandra roy <48654733+codersurjo@users.noreply.github.com>
2022-05-23 18:18:02 +0600

    Responsive complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
-
     // Hero_car
     $('.hero_car').owlCarousel({
         loop: true,
         nav: false,
-        autoplay: false,
+        autoplay: true,
         autoplayHoverPause: true,
         items: 1,
+        animateOut: "fadeOut",
+        animateIn: "fadeIn",
+        autoplayTimeout: 5000,
+        smartSpeed: 6000,
     });
 })
